bbdd09dc4b74b376054cb88a9c7794383da26a38
Constantine VI <constantinevi@outlook.com>
2018-06-29 22:46:58 +0800

    Initial Commit

---
+++ Sentinel Auto Downloader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sentinel Auto Downloader
+//
+//  Created by Constantine VI on 2018/6/29.
+//  Copyright © 2018年 Constantine VI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
