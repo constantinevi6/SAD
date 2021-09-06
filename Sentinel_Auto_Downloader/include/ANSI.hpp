31bef285dcb64480a45b87762ea1c98df9e6851d
Constantine VI <constantinevi@outlook.com>
2021-09-06 17:34:43 +0800

    引入終端機色彩設定

---
+++ Sentinel_Auto_Downloader/include/ANSI.hpp
@@ -0,0 +1,33 @@
+﻿//
+//  ANSI.hpp
+//  Automatic SAR Processing System
+//
+//  Created by Constantine VI on 2020/6/2.
+//  Copyright © 2020 CSRSR. All rights reserved.
+//
+
+#ifndef ANSI_hpp
+#define ANSI_hpp
+
+#define SGR_Reset "\033[0m"
+#define fBlod "\033[1m"
+
+#define fBlack "\033[31m"
+#define fRed "\033[31m"
+#define fGreen "\033[32m"
+#define fYellow "\033[33m"
+#define fBlue "\033[34m"
+#define fMagenta "\033[35m"
+#define fCyan "\033[36m"
+#define fWhite "\033[37m"
+
+#define fBlack_Bright "\033[90m"
+#define fRed_Bright "\033[91m"
+#define fGreen_Bright "\033[92m"
+#define fYellow_Bright "\033[93m"
+#define fBlue_Bright "\033[94m"
+#define fMagenta_Bright "\033[95m"
+#define fCyan_Bright "\033[96m"
+#define fWhite_Bright "\033[97m"
+
+#endif
