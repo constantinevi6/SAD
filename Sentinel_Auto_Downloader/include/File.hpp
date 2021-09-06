849b41f69cda7aa30a750a66cbbeafc71c72086e
Constantine VI <constantinevi@outlook.com>
2021-09-06 17:34:23 +0800

    引入副檔名定義

---
+++ Sentinel_Auto_Downloader/include/File.hpp
@@ -0,0 +1,28 @@
+﻿//
+//  File.hpp
+//  ASPS
+//
+//  Created by Constantine VI on 2021/08/29.
+//  Copyright © 2021 CSRSR. All rights reserved.
+//
+
+#ifndef File_hpp
+#define File_hpp
+
+// 定義副檔名
+#define File_Name_Extension_TXT ".txt"
+
+#define File_Name_Extension_DIMAP ".dim"
+#define File_Name_Extension_ZIP ".zip"
+#define File_Name_Extension_SAFE ".safe"
+#define File_Name_Extension_DATA ".data"
+#define File_Name_Extension_XML ".xml"
+#define File_Name_Extension_TIFF ".tif"
+#define File_Name_Extension_ENVI ".hdr"
+#define File_Name_Extension_IMG ".img"
+#define File_Name_Extension_GAMMA ".par"
+#define File_Name_Extension_GAMMA_RSLC ".rslc"
+#define File_Name_Extension_GAMMA_DIFF ".diff"
+#define File_Name_Extension_GAMMA_BASE ".base"
+
+#endif
